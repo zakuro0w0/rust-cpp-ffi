77a0c92ad2263a87ac43ce53c25042124c4cc255
zakuro0w0 <tatatake0w0@gmail.com>
2023-01-13 21:24:43 +0900

    feat: bindgenで作るrust->c++ FFIのプロジェクトを追加
    - https://github.com/eduidl/rust-ffi-example のコピペ

---
+++ ffi-sys/src/lib.rs
@@ -0,0 +1,21 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(dead_code)]
+
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
