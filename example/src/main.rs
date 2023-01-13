a680aff8f60d91b3a2aa4fe6659727e7a9016a46
zakuro0w0 <tatatake0w0@gmail.com>
2023-01-13 21:46:54 +0900

    build: cargo.tomlの調整

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,4 +1,4 @@
-// use std::os::raw::c_int;
+use std::ffi::{CStr, CString};
 
 // C/C++で実装した関数を宣言する(名前が一致していること)
 extern "C" {
@@ -9,12 +9,27 @@ extern "C" {
     fn hello_world_c() -> i32;
 }
 
-fn main() {
-    println!("Hello, world!");
+// 自前で定義したFFIの呼び出し
+fn self_ffi() {
     // FFIで関数を呼び出す場合はunsafeブロックで囲む必要がある
     // Rustコンパイラはunsafeブロックのメモリ安全性を保障しない
     unsafe {
         println!("return C++ : {}", hello_world_cpp());
         println!("return C : {}", hello_world_c());
     }
+}
+
+// ffi-sysでbindgenを使って定義したFFIの呼び出し
+fn bindgen_ffi() {
+    let c_string = CString::new("Hello FFI").unwrap();
+    unsafe {
+        // ffi-sys/Cargo.tomlで
+        ffi_sys::print_str(c_string.as_ptr());
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+    self_ffi();
+    bindgen_ffi();
 }
